485a58db84b0f104f6f074d500b8c9d571c457e8
almileo <almiron.leonardo@gmail.com>
2025-07-05 20:20:08 -0300

    feat/Routing-models

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { HeroListComponent } from './heroes/pages/hero-list/hero-list';
+import { HeroFormComponent } from './heroes/pages/hero-form/hero-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'heroes', pathMatch: 'full' },
+  { path: 'heroes', component: HeroListComponent },
+  { path: 'heroes/add', component: HeroFormComponent },
+  { path: 'heroes/edit/:id', component: HeroFormComponent },
+];
+
+export const APP_ROUTES = provideRouter(routes);
